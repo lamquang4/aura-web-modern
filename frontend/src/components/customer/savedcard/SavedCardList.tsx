7fc973afe1533cd61e37dc83c03be7217ecdcecd
Quang lam <lamdieuquang0105@gmail.com>
2026-04-28 17:02:04 +0700

    Update SavedCardList.tsx

--- frontend/src/components/customer/savedcard/SavedCardList.tsx
+++ frontend/src/components/customer/savedcard/SavedCardList.tsx
@@ -18,36 +18,32 @@ function SavedCardList() {
       <div className="space-y-[20px]">
         <h2>Thiệp của tôi</h2>
 
-        <div
-          className={`grid grid-cols-1 sm:grid-cols-2 gap-5 ${
-            savedCards.length <= 0 ? "h-[50vh]" : ""
-          }`}
-        >
-          {isLoading ? (
-            <SavedCardSkeletonList />
-          ) : savedCards.length > 0 ? (
-            savedCards.map((savedCard) => (
+        {isLoading ? (
+          <SavedCardSkeletonList />
+        ) : savedCards.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+            {savedCards.map((savedCard) => (
               <SavedCardItem savedCard={savedCard} />
-            ))
-          ) : (
-            <div className="flex justify-center items-center h-[60vh]">
-              <div className="flex flex-col justify-center items-center gap-[15px]">
-                <Image
-                  source={"/assets/empty.png"}
-                  className={"w-[120px]"}
-                  alt={"empty"}
-                  loading="eager"
-                />
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center h-[60vh]">
+            <div className="flex flex-col justify-center items-center gap-[15px]">
+              <Image
+                source={"/assets/empty.png"}
+                className={"w-[120px]"}
+                alt={"empty"}
+                loading="eager"
+              />
 
-                <h4>
-                  Hãy chọn thiệp và viết những lời chúc
-                  <br />
-                  cho người thân, bạn bè của bạn
-                </h4>
-              </div>
+              <h4>
+                Hãy chọn thiệp và viết những lời chúc
+                <br />
+                cho người thân, bạn bè của bạn
+              </h4>
             </div>
-          )}
-        </div>
+          </div>
+        )}
 
         <Pagination
           totalPages={totalPages}
