a541fb4d4e3115a83bc67ab915236164772afac0
Quang lam <lamdieuquang0105@gmail.com>
2026-05-28 19:57:33 +0700

    fix: Update ProfileMenu

--- frontend/src/components/admin/layout/header/ProfileMenu.tsx
+++ frontend/src/components/admin/layout/header/ProfileMenu.tsx
@@ -20,35 +20,35 @@ function ProfileMenu({ menuOpen, onToggleMenu }: Props) {
     <>
       {account && (
         <div
-          className="text-[0.9rem] relative"
+          className="relative"
           onMouseEnter={onToggleMenu}
           onMouseLeave={onToggleMenu}
         >
           <div className="flex cursor-pointer items-center gap-[6px]">
-            <div className="w-[30px] rounded-full border border-gray-300 p-1">
+            <div className="w-[34px] h-[34px] p-1 rounded-full border border-gray-300 overflow-hidden">
               <Image
-                source={"/assets/owner.png"}
+                source="/assets/user.png"
                 alt=""
-                className="w-full"
+                className="w-full h-full object-contain"
                 loading="eager"
               />
             </div>
-            <p>{account.fullname}</p>
+            <p className="font-medium">{account.fullname}</p>
           </div>
 
           {menuOpen && (
             <div
-              className={`absolute top-full right-0 w-[185px] z-20 bg-white shadow-md rounded-md border border-gray-200 transition-all duration-100 origin-top font-medium`}
+              className={`absolute top-full right-0 max-w z-20 bg-white shadow-md rounded-md border border-gray-200 transition-all duration-100 origin-top`}
             >
-              <p className="border-b p-2.5 border-gray-300 max-w-[210px] overflow-hidden text-ellipsis whitespace-nowrap text-center">
+              <p className="border-b p-3 border-gray-300 max-w-[210px] overflow-hidden text-ellipsis whitespace-nowrap text-center font-medium">
                 Xin chào, {account.fullname}
               </p>
 
               <Link
                 to={"/admin/account/profile"}
-                className="block hover:bg-gray-100 px-3 py-3.5"
+                className="block hover:bg-gray-100 p-3 whitespace-nowrap"
               >
-                <div className="flex items-center gap-[8px]">
+                <div className="flex items-center gap-2 font-medium">
                   <CircleUserRound size={18} />
                   <p>Tài khoản</p>
                 </div>
@@ -56,9 +56,9 @@ function ProfileMenu({ menuOpen, onToggleMenu }: Props) {
 
               <Button
                 onClick={logout}
-                className="w-full block hover:bg-gray-100 px-3 py-3.5"
+                className="w-full hover:bg-gray-100 p-3 text-danger whitespace-nowrap"
               >
-                <div className="flex items-center gap-[8px] text-danger font-normal">
+                <div className="flex items-center gap-2 font-medium">
                   <DoorOpen size={18} />
                   <p>Đăng xuất</p>
                 </div>
